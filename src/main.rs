1569533ccfc1ef84bfb2f5739dcf966ad5e7f985
onokatio <onokatio@gmail.com>
2019-05-09 14:45:56 +0900

    run()

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,12 @@ use std::io;
 
 enum OPCODE {
     OP_1,
-    OP_2,
-    OP_DUP,
-    OP_IF,
 }
 
 struct VM {
     stack: Vec<i32>,
     code: Vec<OPCODE>,
-    pc: i32,
+    pc: usize,
 }
 
 impl VM {
@@ -18,10 +15,22 @@ impl VM {
         VM {
             code: code,
             pc: 0,
-            stack: vec![0]
+            stack: vec![]
         }
+    }
+    fn run(&mut self) {
+        match self.code[self.pc] {
+            OPCODE::OP_1 => self.op_1(),
+        }
+    }
+    fn op_1(&mut self){
+        self.stack.push(1);
+        self.pc += 1;
     }
 }
 
 fn main() {
+    let mut vm = VM::new(vec![OPCODE::OP_1]);
+    vm.run();
+    println!("pc:{} stack:{}", vm.pc, vm.stack[0]);
 }
